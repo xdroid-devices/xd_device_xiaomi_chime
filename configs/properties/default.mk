845e60e9402bb7eb166ae68ae611502e43563864
kubersharma001 <kubersharma001@gmail.com>
2021-07-28 12:05:57 +0000

    juice: Unset wide color gamut composition prop
    *  DISPLAY_P3 is not supported.
    
    Change-Id: I0b99fcc69ed99787083fb26781ea6bef3aeb8851
    Signed-off-by: frost. (frostg-012) <frost@nusantararom.org>

--- configs/properties/default.mk
+++ configs/properties/default.mk
@@ -23,8 +23,7 @@ PRODUCT_DEFAULT_PROPERTY_OVERRIDES += \
     ro.surface_flinger.set_display_power_timer_ms=1000 \
     ro.surface_flinger.set_idle_timer_ms=80 \
     ro.surface_flinger.set_touch_timer_ms=200 \
-    ro.surface_flinger.use_color_management=true \
-    ro.surface_flinger.wcg_composition_dataspace=143261696
+    ro.surface_flinger.use_color_management=true
 
 PRODUCT_DEFAULT_PROPERTY_OVERRIDES += \
     debug.sf.late.sf.duration=10500000 \
