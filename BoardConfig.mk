25742d089050deced7db796f78ac402ad10eed28
Adithya R <gh0strider.2k18.reborn@gmail.com>
2021-08-15 21:56:27 +0530

    juice: parts: Add support for LCD HBM and CABC
     * write preferences and qs tiles
     * kang icons from realmeparts
     * enable cabc by default
     * address sepolicy denials
    
    Signed-off-by: Adithya R <gh0strider.2k18.reborn@gmail.com>
    Signed-off-by: Nauval Rizky <enuma.alrizky@gmail.com>
    Signed-off-by: David Setiawan <fryevia@foxmail.com>
    Signed-off-by: frost. (frostg-012) <frost@nusantararom.org>
    Change-Id: I7334d90d7e6f55cecb0d7744d3c8d7f9eda6d537

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -147,6 +147,7 @@ TARGET_COPY_OUT_PRODUCT := product
 TARGET_ODM_PROP += $(DEVICE_PATH)/configs/properties/odm.prop
 TARGET_PRODUCT_PROP += $(DEVICE_PATH)/configs/properties/product.prop
 TARGET_SYSTEM_PROP += $(DEVICE_PATH)/configs/properties/system.prop
+TARGET_SYSTEM_EXT_PROP += $(DEVICE_PATH)/configs/properties/system_ext.prop
 TARGET_VENDOR_PROP += $(DEVICE_PATH)/configs/properties/vendor.prop
 
 # Recovery
