afcc30af565234fafe41fb0cf4639cd2f564beeb
frost (frostg-012) <optimus7907@gmail.com>
2022-04-13 21:22:50 +0700

    juice: Import MIUI offline charging animation
     * Extracted from lime-qssi-user-11-RKQ1.201004.002-V12.5.9.0.RJQMIXM-release-keys
    
    Signed-off-by: frost (frostg-012) <optimus7907@gmail.com>
    Change-Id: If8b3c541dc82d8524a1293b5aa71528f9eb74a09

---
+++ rootdir/charger/charger.mk
@@ -0,0 +1,22 @@
+PRODUCT_COPY_FILES += \
+    device/xiaomi/juice/rootdir/charger/charger:$(TARGET_COPY_OUT_SYSTEM)/bin/charger \
+    device/xiaomi/juice/rootdir/charger/0.png:$(TARGET_OUT_DIR)/root/res/images/charger/0.png \
+    device/xiaomi/juice/rootdir/charger/1.png:$(TARGET_OUT_DIR)/root/res/images/charger/1.png \
+    device/xiaomi/juice/rootdir/charger/2.png:$(TARGET_OUT_DIR)/root/res/images/charger/2.png \
+    device/xiaomi/juice/rootdir/charger/3.png:$(TARGET_OUT_DIR)/root/res/images/charger/3.png \
+    device/xiaomi/juice/rootdir/charger/4.png:$(TARGET_OUT_DIR)/root/res/images/charger/4.png \
+    device/xiaomi/juice/rootdir/charger/5.png:$(TARGET_OUT_DIR)/root/res/images/charger/5.png \
+    device/xiaomi/juice/rootdir/charger/6.png:$(TARGET_OUT_DIR)/root/res/images/charger/6.png \
+    device/xiaomi/juice/rootdir/charger/7.png:$(TARGET_OUT_DIR)/root/res/images/charger/7.png \
+    device/xiaomi/juice/rootdir/charger/8.png:$(TARGET_OUT_DIR)/root/res/images/charger/8.png \
+    device/xiaomi/juice/rootdir/charger/9.png:$(TARGET_OUT_DIR)/root/res/images/charger/9.png \
+    device/xiaomi/juice/rootdir/charger/10.png:$(TARGET_OUT_DIR)/root/res/images/charger/10.png \
+    device/xiaomi/juice/rootdir/charger/Battery.png:$(TARGET_OUT_DIR)/root/res/images/charger/Battery.png \
+    device/xiaomi/juice/rootdir/charger/Battery_0.png:$(TARGET_OUT_DIR)/root/res/images/charger/Battery_0.png \
+    device/xiaomi/juice/rootdir/charger/battery_fail.png:$(TARGET_OUT_DIR)/root/res/images/charger/battery_fail.png \
+    device/xiaomi/juice/rootdir/charger/Battery_green1.png:$(TARGET_OUT_DIR)/root/res/images/charger/Battery_green1.png \
+    device/xiaomi/juice/rootdir/charger/Battery_green2.png:$(TARGET_OUT_DIR)/root/res/images/charger/Battery_green2.png \
+    device/xiaomi/juice/rootdir/charger/Battery_green3.png:$(TARGET_OUT_DIR)/root/res/images/charger/Battery_green3.png \
+    device/xiaomi/juice/rootdir/charger/Battery_red1.png:$(TARGET_OUT_DIR)/root/res/images/charger/Battery_red1.png \
+    device/xiaomi/juice/rootdir/charger/Battery_red2.png:$(TARGET_OUT_DIR)/root/res/images/charger/Battery_red2.png \
+    device/xiaomi/juice/rootdir/charger/battery_scale.png:$(TARGET_OUT_DIR)/root/res/images/charger/battery_scale.png
