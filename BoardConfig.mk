4178e4b86b4b27e160923b33be90de7a8fe4103b
frost. (frostg-012) <optimus7907@gmail.com>
2022-05-22 23:44:05 +0700

    juice: Import framework_compatibility_matrix from MIUI 22.5.18
    Signed-off-by: frost. (frostg-012) <frost@nusantararom.org>
    Change-Id: If26ab1ec01a671f2dc63dd023da34e0a3c8bdca9

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -70,6 +70,7 @@ TARGET_FS_CONFIG_GEN := $(DEVICE_PATH)/configs/config.fs
 # HIDL
 DEVICE_MANIFEST_FILE += $(DEVICE_PATH)/configs/vintf/manifest.xml
 DEVICE_MATRIX_FILE := $(DEVICE_PATH)/configs/vintf/compatibility_matrix.xml
+DEVICE_FRAMEWORK_COMPATIBILITY_MATRIX_FILE := $(DEVICE_PATH)/configs/vintf/framework_compatibility_matrix.xml
 
 ODM_MANIFEST_SKUS += lemon
 ODM_MANIFEST_LEMON_FILES := $(DEVICE_PATH)/configs/vintf/manifest_lemon.xml
