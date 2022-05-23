9a21c292a32dbafbc38bcfbc022f60780fb831d6
HeroBuxx <herobuxx@gmail.com>
2022-05-23 16:05:12 +0000

    juice: Initialize barebone tree
    Signed-off-by: HeroBuxx <herobuxx@gmail.com>
    Signed-off-by: RayapKun <rayapkun@gmail.com>

---
+++ aosp_juice.mk
@@ -0,0 +1,24 @@
+#
+# Copyright (C) 2022 The LineageOS Project
+#
+# SPDX-License-Identifier: Apache-2.0
+#
+
+# Inherit from those products. Most specific first.
+$(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
+$(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
+
+# Inherit from juice device
+$(call inherit-product, device/xiaomi/juice/device.mk)
+
+# Inherit some common rom stuff.
+$(call inherit-product, vendor/aosp/config/common_full_phone.mk)
+
+# Device identifier. This must come after all inclusions.
+PRODUCT_NAME := aosp_juice
+PRODUCT_DEVICE := juice
+PRODUCT_BRAND := Xiaomi
+PRODUCT_MODEL := sm6115
+PRODUCT_MANUFACTURER := Xiaomi
+
+PRODUCT_GMS_CLIENTID_BASE := android-xiaomi
