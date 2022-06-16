4a9d23b4978744a945222487cf3a8f10fc9baa2f
frost. (frostg-012) <frost@nusantararom.org>
2022-06-16 11:00:30 +0700

    juice: Revert back bluetooth audio HIDL to 2.0
    Signed-off-by: frost. (frostg-012) <frost@nusantararom.org>
    Change-Id: Iad19f58ac681bbbd2b52858ab85359a591f49ef2

--- device.mk
+++ device.mk
@@ -41,7 +41,7 @@ PRODUCT_PACKAGES += \
     android.hardware.audio@6.0-impl \
     android.hardware.audio.effect@6.0-impl \
     android.hardware.audio.service \
-    android.hardware.bluetooth.audio@2.1-impl \
+    android.hardware.bluetooth.audio@2.0-impl \
     android.hardware.soundtrigger@2.3-impl \
     audio.a2dp.default \
     audio.bluetooth.default \
@@ -90,7 +90,7 @@ PRODUCT_COPY_FILES += \
 # Bluetooth
 PRODUCT_PACKAGES += \
     android.hardware.bluetooth@1.0.vendor \
-    vendor.qti.hardware.bluetooth_audio@2.1.vendor \
+    vendor.qti.hardware.bluetooth_audio@2.0.vendor \
     vendor.qti.hardware.btconfigstore@1.0.vendor \
     vendor.qti.hardware.btconfigstore@2.0.vendor
 
